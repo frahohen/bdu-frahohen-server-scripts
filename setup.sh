085278c6d79dbe45cfcd175d4538bcf04eb07f5c
frahohen <f.hohenb@gmail.com>
2022-07-03 20:17:32 +0200

    Enable install_ssh_server.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ LOGGER=$(source ${FILE_UTILS} get_absolute_path "" /utils/print_log.sh)
 
 bash "${LOGGER}" info "Execute frahohen-server setup"
 # TODO: install ssh server
-#su root ./install_ssh_server/install_ssh_server.sh
+su root ./install_ssh_server/install_ssh_server.sh
 # TODO: install x2go or vnc
 # TODO: install samba server for network file transfer
 # TODO: install syncTracor required for securing/synching data on my pc  
