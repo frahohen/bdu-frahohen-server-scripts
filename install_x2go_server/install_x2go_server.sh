b4d623947bed8e0b2a84d07a35cd6fd7a9017410
frahohen <f.hohenb@gmail.com>
2022-07-05 22:31:24 +0200

    Add install_x2go_server.sh

---
+++ install_x2go_server/install_x2go_server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# General
+FILE_UTILS="./utils/file_utils.sh"
+LOGGER=$(source ${FILE_UTILS} get_absolute_path install_x2go_server /utils/print_log.sh)
+X2GOSERVER="X2GO Server -"
+
+bash "${LOGGER}" info "${X2GOSERVER} Re-index and fetch all available packages"
+apt-get update -y
+bash "${LOGGER}" info "${X2GOSERVER} Install X2GO server"
+apt install -y xfce4 x2goserver x2goserver-xsession
