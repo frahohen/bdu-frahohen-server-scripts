be79c15bc8e828775bdd4208a59c5866c1fe0ffa
frahohen <f.hohenb@gmail.com>
2022-07-09 12:30:56 +0200

    Change naming of hostname in LOGGER messages

--- home-frahohen.setup.sh
+++ home-frahohen.setup.sh
@@ -3,7 +3,7 @@
 FILE_UTILS="./utils/file_utils.sh"
 LOGGER=$(source ${FILE_UTILS} get_absolute_path "" /utils/print_log.sh)
 
-bash "${LOGGER}" info "Execute home.frahohen setup"
+bash "${LOGGER}" info "Execute home-frahohen setup"
 su root ./install_ssh_server/home-frahohen.install_ssh_server.sh
 su root ./install_x2go_server/install_x2go_server.sh
-bash "${LOGGER}" info "Setup for home.frahohen complete"
+bash "${LOGGER}" info "Setup for home-frahohen complete"
--- server-frahohen.setup.sh
+++ server-frahohen.setup.sh
@@ -3,13 +3,13 @@
 FILE_UTILS="./utils/file_utils.sh"
 LOGGER=$(source ${FILE_UTILS} get_absolute_path "" /utils/print_log.sh)
 
-bash "${LOGGER}" info "Execute server.frahohen setup"
+bash "${LOGGER}" info "Execute server-frahohen setup"
 su root ./install_ssh_server/server-frahohen.install_ssh_server.sh
 su root ./install_x2go_server/install_x2go_server.sh
 su root ./install_syncthing/install_syncthing.sh
 su root ./install_virtualbox_webapp/install_virtualbox_webapp.sh
 su root ./install_nut_server/install_nut_server.sh
-bash "${LOGGER}" info "Setup for server.frahohen complete"
+bash "${LOGGER}" info "Setup for server-frahohen complete"
 
 # Reboot is required for the nut server setup
 echo "Reboot required to complete nut server setup. Do you want to reboot now?"
