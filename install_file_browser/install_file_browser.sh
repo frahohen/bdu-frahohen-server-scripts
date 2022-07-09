7877f9eafa76c8f6893c7db00d9590795e3465ce
frahohen <f.hohenb@gmail.com>
2022-07-10 00:06:08 +0200

    Fix typo in install_file_browser.sh

--- install_file_browser/install_file_browser.sh
+++ install_file_browser/install_file_browser.sh
@@ -12,6 +12,6 @@ curl -fsSL https://raw.githubusercontent.com/filebrowser/get/master/get.sh | bas
 
 bash "${LOGGER}" info "${FILEBROWSER} Create service and start File Browser"
 rm -f /etc/systemd/system/filebrowser.service
-cp -f ./install_nut_server/resources/etc/systemd/system/filebrowser.service /etc/systemd/system/
+cp -f ./install_file_browser/resources/etc/systemd/system/filebrowser.service /etc/systemd/system/
 systemctl enable filebrowser.service
 systemctl start filebrowser.service
